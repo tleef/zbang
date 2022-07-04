e0bc57a7f2bbc016468a1ef1c0c5dc8637cecec2
Thomas Lee <tom@tleef.com>
2022-07-04 12:00:04 -0400

    Add Float support

---
+++ lib/types/float.ex
@@ -0,0 +1,219 @@
+defmodule Bliss.Float do
+  alias Bliss.{Result, Error, Any}
+
+  use Bliss.Type,
+    options:
+      Bliss.Any.__bliss__(:options) ++ [:parse, :allow_int, :min, :max, :greater_than, :less_than]
+
+  def check(result, rules, context) do
+    result
+    |> Any.check(rules, context)
+    |> maybe_check(:parse, rules, context)
+    |> maybe_check(:allow_int, rules, context)
+    |> check(:type, rules, context)
+    |> maybe_check(:min, rules, context)
+    |> maybe_check(:max, rules, context)
+    |> maybe_check(:greater_than, rules, context)
+    |> maybe_check(:less_than, rules, context)
+  end
+
+  def check(%Result{value: nil} = result, _rule, _options, _context) do
+    result
+  end
+
+  def check(result, :parse, _enabled, _context) when not is_binary(result.value) do
+    result
+  end
+
+  def check(result, :parse, false, _context) do
+    result
+  end
+
+  def check(result, :parse, true, context) do
+    case Float.parse(result.value) do
+      {float, _} ->
+        result |> Result.set_value(float)
+
+      :error ->
+        message = "unable to parse input as a float"
+
+        result
+        |> Result.add_error(
+          Error.new(
+            Error.Codes.invalid_string(),
+            message,
+            context
+          )
+        )
+    end
+  end
+
+  def check(result, :allow_int, _enabled, _context) when not is_integer(result.value) do
+    result
+  end
+
+  def check(result, :allow_int, false, _context) do
+    result
+  end
+
+  def check(result, :allow_int, true, _context) do
+    result |> Result.set_value(result.value / 1)
+  end
+
+  def check(result, :type, options, context) when not is_float(result.value) do
+    message = Keyword.get(options, :message, "input is not a float")
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_type(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :type, _options, _context) do
+    result
+  end
+
+  def check(result, _rule, _options, _context) when not is_float(result.value) do
+    result
+  end
+
+  def check(result, :min, {value, _options}, context) when not is_float(value) do
+    message = "min value must be a float"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :min, {value, options}, context) when result.value < value do
+    message = Keyword.get(options, :message, "input is too small")
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.too_small(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :min, {_value, _options}, _context) do
+    result
+  end
+
+  def check(result, :min, value, context) do
+    check(result, :min, {value, []}, context)
+  end
+
+  def check(result, :max, {value, _options}, context) when not is_float(value) do
+    message = "max value must be a float"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :max, {value, options}, context) when result.value > value do
+    message = Keyword.get(options, :message, "input is too big")
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.too_big(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :max, {_value, _options}, _context) do
+    result
+  end
+
+  def check(result, :max, value, context) do
+    check(result, :max, {value, []}, context)
+  end
+
+  def check(result, :greater_than, {value, _options}, context) when not is_float(value) do
+    message = "greater_than value must be a float"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :greater_than, {value, options}, context) when result.value <= value do
+    message = Keyword.get(options, :message, "input is too small")
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.too_small(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :greater_than, {_value, _options}, _context) do
+    result
+  end
+
+  def check(result, :greater_than, value, context) do
+    check(result, :greater_than, {value, []}, context)
+  end
+
+  def check(result, :less_than, {value, _options}, context) when not is_float(value) do
+    message = "less_than value must be a float"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :less_than, {value, options}, context) when result.value >= value do
+    message = Keyword.get(options, :message, "input is too big")
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.too_big(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :less_than, {_value, _options}, _context) do
+    result
+  end
+
+  def check(result, :less_than, value, context) do
+    check(result, :less_than, {value, []}, context)
+  end
+end
---
+++ test/types/float_test.exs
@@ -0,0 +1,349 @@
+defmodule Bliss.Float.Test do
+  use ExUnit.Case, async: true
+
+  alias Bliss.{Result, Error, Context, Float}
+
+  describe "Bliss.Float.check(_, :parse, _, _)/4" do
+    test "given `true`, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> Float.check(:parse, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some float, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(10.5)
+        |> Float.check(:parse, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some float string, returns result with parsed value" do
+      result =
+        Result.new()
+        |> Result.set_value("10.5")
+        |> Float.check(:parse, true, Context.new("."))
+
+      assert result.value == 10.5
+    end
+
+    test "given `true`, when some integer string, returns result with parsed value" do
+      result =
+        Result.new()
+        |> Result.set_value("10")
+        |> Float.check(:parse, true, Context.new("."))
+
+      assert result.value == 10.0
+    end
+
+    test "given `false`, when some value, returns result with original value" do
+      result =
+        Result.new()
+        |> Result.set_value("10.5")
+        |> Float.check(:parse, false, Context.new("."))
+
+      assert result.value == "10.5"
+    end
+
+    test "given `true`, when some invalid string, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value("oops")
+        |> Float.check(:parse, true, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_string(),
+               message: "unable to parse input as a float",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.Float.check(_, :allow_int, _, _)/4" do
+    test "given `true`, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> Float.check(:allow_int, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some float, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(32.5)
+        |> Float.check(:allow_int, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some int, returns result with converted value" do
+      result =
+        Result.new()
+        |> Result.set_value(10)
+        |> Float.check(:allow_int, true, Context.new("."))
+
+      assert result.value == 10.0
+    end
+
+    test "given `false`, when some value, returns result with original value" do
+      result =
+        Result.new()
+        |> Result.set_value(32)
+        |> Float.check(:allow_int, false, Context.new("."))
+
+      assert result.value == 32
+    end
+  end
+
+  describe "Bliss.Float.check(_, :type, _, _)/4" do
+    test "given empty options, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> Float.check(:type, [], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given empty options, when float value, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(123.4)
+        |> Float.check(:type, [], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given empty options, when string value, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value("123.4")
+        |> Float.check(:type, [], Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_type(),
+               message: "input is not a float",
+               path: ["."]
+             })
+    end
+
+    test "given empty options, when int value, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(34)
+        |> Float.check(:type, [], Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_type(),
+               message: "input is not a float",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.Float.check(_, :min, _, _)/4" do
+    test "given min value, when not a float, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(3)
+        |> Float.check(:min, 11, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given min value, when great enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(11.0)
+        |> Float.check(:min, 11.0, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given min value, when too small, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(5.9)
+        |> Float.check(:min, 6.0, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.too_small(),
+               message: "input is too small",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.Float.check(_, :max, _, _)/4" do
+    test "given max value, when not a float, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(13)
+        |> Float.check(:max, 11.0, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given max value, when small enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(11.0)
+        |> Float.check(:max, 11.0, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given max value, when too big, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(6.1)
+        |> Float.check(:max, 6.0, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.too_big(),
+               message: "input is too big",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.Float.check(_, :greater_than, _, _)/4" do
+    test "given greater than value, when not a float, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(8)
+        |> Float.check(:greater_than, 11.0, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given greater than value, when great enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(11.1)
+        |> Float.check(:greater_than, 11.0, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given greater than value, when too small, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(6.0)
+        |> Float.check(:greater_than, 6.0, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.too_small(),
+               message: "input is too small",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.Float.check(_, :less_than, _, _)/4" do
+    test "given less than value, when not a float, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(12)
+        |> Float.check(:less_than, 11.0, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given less than value, when small enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(10.9)
+        |> Float.check(:less_than, 11.0, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given less than value, when too big, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(6.0)
+        |> Float.check(:less_than, 6.0, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.too_big(),
+               message: "input is too big",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.Float.validate/3" do
+    test "given some float string value, when :parse, set parsed value" do
+      {:ok, 10.5} = Float.validate("10.5", [:parse])
+    end
+
+    test "given some int value, when :allow_int, set truncated value" do
+      {:ok, 10.0} = Float.validate(10, [:allow_int])
+    end
+
+    test "given small value, when check :min, check min" do
+      {:error, errors} = Float.validate(20.9, min: {21.0, message: "too small"})
+
+      assert Enum.member?(errors, %Error{
+               code: Error.Codes.too_small(),
+               message: "too small",
+               path: ["."]
+             })
+    end
+
+    test "given large value, when check :max, check max" do
+      {:error, errors} = Float.validate(17.1, max: {17.0, message: "too great"})
+
+      assert Enum.member?(errors, %Error{
+               code: Error.Codes.too_big(),
+               message: "too great",
+               path: ["."]
+             })
+    end
+
+    test "given small value, when check :greater_than, check greater_than" do
+      {:error, errors} = Float.validate(21.0, greater_than: {21.0, message: "not great enough"})
+
+      assert Enum.member?(errors, %Error{
+               code: Error.Codes.too_small(),
+               message: "not great enough",
+               path: ["."]
+             })
+    end
+
+    test "given large value, when check :less_than, check less_than" do
+      {:error, errors} = Float.validate(18.0, less_than: {18.0, message: "not small enough"})
+
+      assert Enum.member?(errors, %Error{
+               code: Error.Codes.too_big(),
+               message: "not small enough",
+               path: ["."]
+             })
+    end
+
+    test "given some float string, when :parse and check :min & :max, parse value and check min and max" do
+      {:ok, 23.0} = Float.validate("23", [:parse, min: 0.0, max: 25.0])
+    end
+  end
+end
