b2b7d639fc6905613f65e841b88dc88d79bdeed7
Thomas Lee <tom@tleef.com>
2022-07-04 02:06:28 -0400

    Add :trunc and fix some edge cases (#10)

--- lib/types/integer.ex
+++ lib/types/integer.ex
@@ -2,12 +2,14 @@ defmodule Bliss.Integer do
   alias Bliss.{Result, Error, Any}
 
   use Bliss.Type,
-    options: Bliss.Any.__bliss__(:options) ++ [:parse, :min, :max, :greater_than, :less_than]
+    options:
+      Bliss.Any.__bliss__(:options) ++ [:parse, :trunc, :min, :max, :greater_than, :less_than]
 
   def check(result, rules, context) do
     result
     |> Any.check(rules, context)
     |> maybe_check(:parse, rules, context)
+    |> maybe_check(:trunc, rules, context)
     |> check(:type, rules, context)
     |> maybe_check(:min, rules, context)
     |> maybe_check(:max, rules, context)
@@ -19,6 +21,10 @@ defmodule Bliss.Integer do
     result
   end
 
+  def check(result, :parse, _base, _context) when not is_binary(result.value) do
+    result
+  end
+
   def check(result, :parse, false, _context) do
     result
   end
@@ -27,9 +33,48 @@ defmodule Bliss.Integer do
     check(result, :parse, 10, context)
   end
 
-  def check(result, :parse, base, _context) when is_binary(result.value) and base in 2..36 do
-    {int, _} = result.value |> Integer.parse(base)
-    result |> Result.set_value(int)
+  def check(result, :parse, base, context) when base not in 2..36 do
+    message = "unable to parse integer with base #{inspect(base)}, base must be in 2..36"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :parse, base, context) do
+    case Integer.parse(result.value, base) do
+      {int, _} ->
+        result |> Result.set_value(int)
+
+      :error ->
+        message = "unable to parse input as an integer"
+
+        result
+        |> Result.add_error(
+          Error.new(
+            Error.Codes.invalid_string(),
+            message,
+            context
+          )
+        )
+    end
+  end
+
+  def check(result, :trunc, _enabled, _context) when not is_float(result.value) do
+    result
+  end
+
+  def check(result, :trunc, false, _context) do
+    result
+  end
+
+  def check(result, :trunc, true, _context) do
+    result |> Result.set_value(trunc(result.value))
   end
 
   def check(result, :type, options, context) when not is_integer(result.value) do
@@ -49,8 +94,24 @@ defmodule Bliss.Integer do
     result
   end
 
-  def check(result, :min, {value, options}, context)
-      when is_integer(value) and result.value < value do
+  def check(result, _rule, _options, _context) when not is_integer(result.value) do
+    result
+  end
+
+  def check(result, :min, {value, _options}, context) when not is_integer(value) do
+    message = "min value must be an integer"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :min, {value, options}, context) when result.value < value do
     message = Keyword.get(options, :message, "input is too small")
 
     result
@@ -63,16 +124,28 @@ defmodule Bliss.Integer do
     )
   end
 
-  def check(result, :min, {value, _options}, _context) when is_integer(value) do
+  def check(result, :min, {_value, _options}, _context) do
     result
   end
 
-  def check(result, :min, value, context) when is_integer(value) do
+  def check(result, :min, value, context) do
     check(result, :min, {value, []}, context)
   end
 
-  def check(result, :max, {value, options}, context)
-      when is_integer(value) and result.value > value do
+  def check(result, :max, {value, _options}, context) when not is_integer(value) do
+    message = "max value must be an integer"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :max, {value, options}, context) when result.value > value do
     message = Keyword.get(options, :message, "input is too big")
 
     result
@@ -85,16 +158,28 @@ defmodule Bliss.Integer do
     )
   end
 
-  def check(result, :max, {value, _options}, _context) when is_integer(value) do
+  def check(result, :max, {_value, _options}, _context) do
     result
   end
 
-  def check(result, :max, value, context) when is_integer(value) do
+  def check(result, :max, value, context) do
     check(result, :max, {value, []}, context)
   end
 
-  def check(result, :greater_than, {value, options}, context)
-      when is_integer(value) and result.value <= value do
+  def check(result, :greater_than, {value, _options}, context) when not is_integer(value) do
+    message = "greater_than value must be an integer"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :greater_than, {value, options}, context) when result.value <= value do
     message = Keyword.get(options, :message, "input is too small")
 
     result
@@ -107,16 +192,28 @@ defmodule Bliss.Integer do
     )
   end
 
-  def check(result, :greater_than, {value, _options}, _context) when is_integer(value) do
+  def check(result, :greater_than, {_value, _options}, _context) do
     result
   end
 
-  def check(result, :greater_than, value, context) when is_integer(value) do
+  def check(result, :greater_than, value, context) do
     check(result, :greater_than, {value, []}, context)
   end
 
-  def check(result, :less_than, {value, options}, context)
-      when is_integer(value) and result.value >= value do
+  def check(result, :less_than, {value, _options}, context) when not is_integer(value) do
+    message = "less_than value must be an integer"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :less_than, {value, options}, context) when result.value >= value do
     message = Keyword.get(options, :message, "input is too big")
 
     result
@@ -129,11 +226,11 @@ defmodule Bliss.Integer do
     )
   end
 
-  def check(result, :less_than, {value, _options}, _context) when is_integer(value) do
+  def check(result, :less_than, {_value, _options}, _context) do
     result
   end
 
-  def check(result, :less_than, value, context) when is_integer(value) do
+  def check(result, :less_than, value, context) do
     check(result, :less_than, {value, []}, context)
   end
 end
--- test/types/integer_test.exs
+++ test/types/integer_test.exs
@@ -13,6 +13,15 @@ defmodule Bliss.Integer.Test do
       assert result.status == :valid
     end
 
+    test "given `true`, when some int, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(10)
+        |> Integer.check(:parse, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
     test "given `true`, when some integer string, returns result with parsed value" do
       result =
         Result.new()
@@ -48,6 +57,74 @@ defmodule Bliss.Integer.Test do
 
       assert result.value == 244
     end
+
+    test "given invalid base, when some integer string, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value("10")
+        |> Integer.check(:parse, 42, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_arguments(),
+               message: "unable to parse integer with base 42, base must be in 2..36",
+               path: ["."]
+             })
+    end
+
+    test "given `true`, when some invalid string, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value("oops")
+        |> Integer.check(:parse, true, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_string(),
+               message: "unable to parse input as an integer",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.Integer.check(_, :trunc, _, _)/4" do
+    test "given `true`, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> Integer.check(:trunc, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some int, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(10)
+        |> Integer.check(:trunc, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some float, returns result with truncated value" do
+      result =
+        Result.new()
+        |> Result.set_value(32.5)
+        |> Integer.check(:trunc, true, Context.new("."))
+
+      assert result.value == 32
+    end
+
+    test "given `false`, when some value, returns result with original value" do
+      result =
+        Result.new()
+        |> Result.set_value(32.5)
+        |> Integer.check(:trunc, false, Context.new("."))
+
+      assert result.value == 32.5
+    end
   end
 
   describe "Bliss.Integer.check(_, :type, _, _)/4" do
@@ -55,6 +132,15 @@ defmodule Bliss.Integer.Test do
       result =
         Result.new()
         |> Result.set_value(nil)
+        |> Integer.check(:type, [], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given empty options, when integer value, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(123)
         |> Integer.check(:type, [], Context.new("."))
 
       assert result.status == :valid
@@ -75,15 +161,6 @@ defmodule Bliss.Integer.Test do
              })
     end
 
-    test "given empty options, when integer value, returns valid result" do
-      result =
-        Result.new()
-        |> Result.set_value(123)
-        |> Integer.check(:type, [], Context.new("."))
-
-      assert result.status == :valid
-    end
-
     test "given empty options, when float value, returns invalid result with error" do
       result =
         Result.new()
@@ -101,6 +178,24 @@ defmodule Bliss.Integer.Test do
   end
 
   describe "Bliss.Integer.check(_, :min, _, _)/4" do
+    test "given min value, when not an int, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(3.5)
+        |> Integer.check(:min, 11, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given min value, when great enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(11)
+        |> Integer.check(:min, 11, Context.new("."))
+
+      assert result.status == :valid
+    end
+
     test "given min value, when too small, returns invalid result with error" do
       result =
         Result.new()
@@ -115,18 +210,27 @@ defmodule Bliss.Integer.Test do
                path: ["."]
              })
     end
-
-    test "given min value, when great enough, returns valid result" do
-      result =
-        Result.new()
-        |> Result.set_value(11)
-        |> Integer.check(:min, 11, Context.new("."))
-
-      assert result.status == :valid
-    end
   end
 
   describe "Bliss.Integer.check(_, :max, _, _)/4" do
+    test "given max value, when not an int, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(13.5)
+        |> Integer.check(:max, 11, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given max value, when small enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(11)
+        |> Integer.check(:max, 11, Context.new("."))
+
+      assert result.status == :valid
+    end
+
     test "given max value, when too big, returns invalid result with error" do
       result =
         Result.new()
@@ -141,18 +245,27 @@ defmodule Bliss.Integer.Test do
                path: ["."]
              })
     end
-
-    test "given max value, when small enough, returns valid result" do
-      result =
-        Result.new()
-        |> Result.set_value(11)
-        |> Integer.check(:max, 11, Context.new("."))
-
-      assert result.status == :valid
-    end
   end
 
   describe "Bliss.Integer.check(_, :greater_than, _, _)/4" do
+    test "given greater than value, when not an int, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(8.5)
+        |> Integer.check(:greater_than, 11, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given greater than value, when great enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(12)
+        |> Integer.check(:greater_than, 11, Context.new("."))
+
+      assert result.status == :valid
+    end
+
     test "given greater than value, when too small, returns invalid result with error" do
       result =
         Result.new()
@@ -167,18 +280,27 @@ defmodule Bliss.Integer.Test do
                path: ["."]
              })
     end
-
-    test "given greater than value, when great enough, returns valid result" do
-      result =
-        Result.new()
-        |> Result.set_value(12)
-        |> Integer.check(:greater_than, 11, Context.new("."))
-
-      assert result.status == :valid
-    end
   end
 
   describe "Bliss.Integer.check(_, :less_than, _, _)/4" do
+    test "given less than value, when not an int, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(12.5)
+        |> Integer.check(:less_than, 11, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given less than value, when small enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(10)
+        |> Integer.check(:less_than, 11, Context.new("."))
+
+      assert result.status == :valid
+    end
+
     test "given less than value, when too big, returns invalid result with error" do
       result =
         Result.new()
@@ -193,20 +315,15 @@ defmodule Bliss.Integer.Test do
                path: ["."]
              })
     end
-
-    test "given less than value, when small enough, returns valid result" do
-      result =
-        Result.new()
-        |> Result.set_value(10)
-        |> Integer.check(:less_than, 11, Context.new("."))
-
-      assert result.status == :valid
-    end
   end
 
   describe "Bliss.Integer.validate/3" do
-    test "given some integer string value, when :parse value, set parsed value" do
+    test "given some integer string value, when :parse, set parsed value" do
       {:ok, 10} = Integer.validate("10", [:parse])
+    end
+
+    test "given some float value, when :trunc, set truncated value" do
+      {:ok, 10} = Integer.validate(10.5, [:trunc])
     end
 
     test "given small value, when check :min, check min" do
