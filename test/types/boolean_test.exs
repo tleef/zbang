7b9723aaf3111b5ae82921dcf48d9c33d28b6f70
Thomas Lee <tom@tleef.com>
2022-07-04 13:04:12 -0400

    Add Boolean support (#14)

---
+++ test/types/boolean_test.exs
@@ -0,0 +1,128 @@
+defmodule Bliss.Boolean.Test do
+  use ExUnit.Case, async: true
+
+  alias Bliss.{Result, Error, Context, Boolean}
+
+  describe "Bliss.Boolean.check(_, :parse, _, _)/4" do
+    test "given `true`, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> Boolean.check(:parse, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some boolean, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(true)
+        |> Boolean.check(:parse, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when \"true\", returns result with parsed value" do
+      result =
+        Result.new()
+        |> Result.set_value("true")
+        |> Boolean.check(:parse, true, Context.new("."))
+
+      assert result.value == true
+    end
+
+    test "given `true`, when \"FALSE\", returns result with parsed value" do
+      result =
+        Result.new()
+        |> Result.set_value("FALSE")
+        |> Boolean.check(:parse, true, Context.new("."))
+
+      assert result.value == false
+    end
+
+    test "given `false`, when some value, returns result with original value" do
+      result =
+        Result.new()
+        |> Result.set_value("TRUE")
+        |> Boolean.check(:parse, false, Context.new("."))
+
+      assert result.value == "TRUE"
+    end
+
+    test "given `true`, when some invalid string, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value("oops")
+        |> Boolean.check(:parse, true, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_string(),
+               message: "unable to parse input as a boolean",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.Boolean.check(_, :type, _, _)/4" do
+    test "given empty options, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> Boolean.check(:type, [], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given empty options, when boolean value, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(true)
+        |> Boolean.check(:type, [], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given empty options, when string value, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value("true")
+        |> Boolean.check(:type, [], Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_type(),
+               message: "input is not a boolean",
+               path: ["."]
+             })
+    end
+
+    test "given empty options, when int value, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(1)
+        |> Boolean.check(:type, [], Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_type(),
+               message: "input is not a boolean",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.Boolean.validate/3" do
+    test "given some boolean string value, when :parse, set parsed value" do
+      {:ok, true} = Boolean.validate("true", [:parse])
+      {:ok, true} = Boolean.validate("True", [:parse])
+      {:ok, true} = Boolean.validate("TRUE", [:parse])
+      {:ok, false} = Boolean.validate("false", [:parse])
+      {:ok, false} = Boolean.validate("False", [:parse])
+      {:ok, false} = Boolean.validate("FALSE", [:parse])
+    end
+  end
+end
