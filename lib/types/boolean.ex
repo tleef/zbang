4058731d6286226dd14836d2ab433b6ab8fd9769
Thomas Lee <tom@tleef.com>
2022-07-04 13:01:37 -0400

    Add Boolean support

---
+++ lib/types/boolean.ex
@@ -0,0 +1,64 @@
+defmodule Bliss.Boolean do
+  alias Bliss.{Result, Error, Any}
+
+  use Bliss.Type,
+    options: Bliss.Any.__bliss__(:options) ++ [:parse]
+
+  def check(result, rules, context) do
+    result
+    |> Any.check(rules, context)
+    |> maybe_check(:parse, rules, context)
+    |> check(:type, rules, context)
+  end
+
+  def check(%Result{value: nil} = result, _rule, _options, _context) do
+    result
+  end
+
+  def check(result, :parse, _enabled, _context) when not is_binary(result.value) do
+    result
+  end
+
+  def check(result, :parse, false, _context) do
+    result
+  end
+
+  def check(result, :parse, true, context) do
+    case String.upcase(result.value) do
+      "TRUE" ->
+        result |> Result.set_value(true)
+
+      "FALSE" ->
+        result |> Result.set_value(false)
+
+      _ ->
+        message = "unable to parse input as a boolean"
+
+        result
+        |> Result.add_error(
+          Error.new(
+            Error.Codes.invalid_string(),
+            message,
+            context
+          )
+        )
+    end
+  end
+
+  def check(result, :type, options, context) when not is_boolean(result.value) do
+    message = Keyword.get(options, :message, "input is not a boolean")
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_type(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :type, _options, _context) do
+    result
+  end
+end
