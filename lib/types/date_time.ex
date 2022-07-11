d0eae94c34b0e0032b1b92b6a260cf13d088b137
Thomas Lee <tom@tleef.com>
2022-07-11 01:07:01 -0400

    Add DateTime (#21)
    * Progress on DateTime
    
    * Add :shift and :trunc support
    
    * Add :min and :max
    
    * Add :parse tests
    
    * Add :allow_int tests
    
    * Add :type tests
    
    * Add :shift tests
    
    * Add :trunc tests
    
    * Add min and max tests
    
    * Add validation tests

---
+++ lib/types/date_time.ex
@@ -0,0 +1,279 @@
+defmodule Bliss.DateTime do
+  alias Bliss.{Result, Error, Any}
+
+  use Bliss.Type,
+    options: Bliss.Any.__bliss__(:options) ++ [:parse, :allow_int, :shift, :trunc, :min, :max]
+
+  def check(result, rules, context) do
+    result
+    |> Any.check(rules, context)
+    |> maybe_check(:parse, rules, context)
+    |> maybe_check(:allow_int, rules, context)
+    |> check(:type, rules, context)
+    |> maybe_check(:shift, rules, context)
+    |> maybe_check(:trunc, rules, context)
+    |> maybe_check(:min, rules, context)
+    |> maybe_check(:max, rules, context)
+  end
+
+  def check(result, _rule, _options, _context) when result.value == nil do
+    result
+  end
+
+  def check(result, :parse, _format, _context) when not is_binary(result.value) do
+    result
+  end
+
+  def check(result, :parse, false, _context) do
+    result
+  end
+
+  def check(result, :parse, true, context) do
+    check(result, :parse, :iso8601, context)
+  end
+
+  def check(result, :parse, format, context) when format not in [:iso8601] do
+    message = "unable to parse DateTime with format: #{inspect(format)}, format must be :iso8601"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :parse, _format, context) do
+    case DateTime.from_iso8601(result.value) do
+      {:ok, datetime, _offset} ->
+        result |> Result.set_value(datetime)
+
+      {:error, _} ->
+        message = "unable to parse input as a DateTime"
+
+        result
+        |> Result.add_error(
+          Error.new(
+            Error.Codes.invalid_string(),
+            message,
+            context
+          )
+        )
+    end
+  end
+
+  def check(result, :allow_int, _mode, _context) when not is_integer(result.value) do
+    result
+  end
+
+  def check(result, :allow_int, false, _context) do
+    result
+  end
+
+  def check(result, :allow_int, true, context) do
+    check(result, :allow_int, :unix, context)
+  end
+
+  def check(result, :allow_int, mode, context) when mode not in [:unix, :gregorian] do
+    message =
+      "unable to convert to DateTime with mode: #{inspect(mode)}, mode must be :unix or :gregorian"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :allow_int, mode, context) do
+    case mode do
+      :unix ->
+        from_unix(result, context)
+
+      :gregorian ->
+        from_gregorian(result, context)
+    end
+  end
+
+  def check(result, :type, options, context) when not is_struct(result.value, DateTime) do
+    message = Keyword.get(options, :message, "input is not a DateTime")
+
+    result
+    |> Bliss.Result.add_error(
+      Bliss.Error.new(
+        Bliss.Error.Codes.invalid_type(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :type, _options, _context) do
+    result
+  end
+
+  def check(result, _rule, _options, _context) when not is_struct(result.value, DateTime) do
+    result
+  end
+
+  def check(result, :shift, zone, context) when not is_binary(zone) do
+    message = "timezone must be a string"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :shift, zone, context) do
+    case DateTime.shift_zone(result.value, zone) do
+      {:ok, datetime} ->
+        result |> Result.set_value(datetime)
+
+      {:error, _} ->
+        message = "unable to shift input to #{inspect(zone)} timezone"
+
+        result
+        |> Result.add_error(
+          Error.new(
+            Error.Codes.invalid_arguments(),
+            message,
+            context
+          )
+        )
+    end
+  end
+
+  def check(result, :trunc, false, _context) do
+    result
+  end
+
+  def check(result, :trunc, true, context) do
+    check(result, :trunc, :second, context)
+  end
+
+  def check(result, :trunc, precision, context)
+      when precision not in [:second, :millisecond, :microsecond] do
+    message =
+      "unable to truncate with precision: #{inspect(precision)}, precision must be :second, :millisecond, or :microsecond"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :trunc, precision, _context) do
+    result |> Result.set_value(result.value |> DateTime.truncate(precision))
+  end
+
+  def check(result, :min, {value, _options}, context) when not is_struct(value, DateTime) do
+    message = "min value must be a DateTime"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :min, {value, options}, context) do
+    case DateTime.compare(result.value, value) do
+      :lt ->
+        message = Keyword.get(options, :message, "input is too early")
+
+        result
+        |> Result.add_error(
+          Error.new(
+            Error.Codes.too_small(),
+            message,
+            context
+          )
+        )
+
+      _ ->
+        result
+    end
+  end
+
+  def check(result, :min, value, context) do
+    check(result, :min, {value, []}, context)
+  end
+
+  def check(result, :max, {value, _options}, context) when not is_struct(value, DateTime) do
+    message = "max value must be a DateTime"
+
+    result
+    |> Result.add_error(
+      Error.new(
+        Error.Codes.invalid_arguments(),
+        message,
+        context
+      )
+    )
+  end
+
+  def check(result, :max, {value, options}, context) do
+    case DateTime.compare(result.value, value) do
+      :gt ->
+        message = Keyword.get(options, :message, "input is too late")
+
+        result
+        |> Result.add_error(
+          Error.new(
+            Error.Codes.too_big(),
+            message,
+            context
+          )
+        )
+
+      _ ->
+        result
+    end
+  end
+
+  def check(result, :max, value, context) do
+    check(result, :max, {value, []}, context)
+  end
+
+  defp from_unix(result, context) do
+    case DateTime.from_unix(result.value) do
+      {:ok, datetime} ->
+        result |> Result.set_value(datetime)
+
+      {:error, _} ->
+        message = "unable to convert unix input to a DateTime"
+
+        result
+        |> Result.add_error(
+          Error.new(
+            Error.Codes.invalid_date(),
+            message,
+            context
+          )
+        )
+    end
+  end
+
+  defp from_gregorian(result, _context) do
+    datetime = DateTime.from_gregorian_seconds(result.value)
+
+    result |> Result.set_value(datetime)
+  end
+end
---
+++ test/types/date_time_test.exs
@@ -0,0 +1,491 @@
+defmodule Bliss.DateTime.Test do
+  use ExUnit.Case, async: true
+
+  alias Bliss.{Result, Error, Context, DateTime}
+
+  describe "Bliss.DateTime.check(_, :parse, _, _)/4" do
+    test "given `true`, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> DateTime.check(:parse, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some DateTime, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2016-05-24 13:26:08Z])
+        |> DateTime.check(:parse, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some ISO 8601 string, returns result with parsed value" do
+      result =
+        Result.new()
+        |> Result.set_value("2015-01-23T23:50:07Z")
+        |> DateTime.check(:parse, true, Context.new("."))
+
+      assert result.value == ~U[2015-01-23 23:50:07Z]
+    end
+
+    test "given `true`, when some ISO 8601 string with offset, returns result with parsed UTC value" do
+      result =
+        Result.new()
+        |> Result.set_value("2015-01-23T23:50:07.123+02:30")
+        |> DateTime.check(:parse, true, Context.new("."))
+
+      assert result.value == ~U[2015-01-23 21:20:07.123Z]
+    end
+
+    test "given `false`, when some value, returns result with original value" do
+      result =
+        Result.new()
+        |> Result.set_value("2015-01-23T23:50:07Z")
+        |> DateTime.check(:parse, false, Context.new("."))
+
+      assert result.value == "2015-01-23T23:50:07Z"
+    end
+
+    test "given format :iso8601, when some ISO 8601 string, returns result with parsed value" do
+      result =
+        Result.new()
+        |> Result.set_value("2015-01-23T23:50:07Z")
+        |> DateTime.check(:parse, :iso8601, Context.new("."))
+
+      assert result.value == ~U[2015-01-23 23:50:07Z]
+    end
+
+    test "given invalid format, when some ISO 8601 string, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value("2015-01-23T23:50:07Z")
+        |> DateTime.check(:parse, :invalid, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_arguments(),
+               message: "unable to parse DateTime with format: :invalid, format must be :iso8601",
+               path: ["."]
+             })
+    end
+
+    test "given `true`, when some invalid string, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value("2015-01-23T23:50:07")
+        |> DateTime.check(:parse, true, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_string(),
+               message: "unable to parse input as a DateTime",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.DateTime.check(_, :allow_int, _, _)/4" do
+    test "given `true`, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> DateTime.check(:allow_int, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some DateTime, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2016-05-24 13:26:08Z])
+        |> DateTime.check(:allow_int, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some unix timestamp, returns result with converted value" do
+      result =
+        Result.new()
+        |> Result.set_value(1_464_096_368)
+        |> DateTime.check(:allow_int, true, Context.new("."))
+
+      assert result.value == ~U[2016-05-24 13:26:08Z]
+    end
+
+    test "given :unix, when some unix timestamp, returns result with converted value" do
+      result =
+        Result.new()
+        |> Result.set_value(1_464_096_368)
+        |> DateTime.check(:allow_int, :unix, Context.new("."))
+
+      assert result.value == ~U[2016-05-24 13:26:08Z]
+    end
+
+    test "given :unix, when some invalid unix timestamp, returns invalid result with errors" do
+      result =
+        Result.new()
+        |> Result.set_value(253_402_300_800)
+        |> DateTime.check(:allow_int, :unix, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_date(),
+               message: "unable to convert unix input to a DateTime",
+               path: ["."]
+             })
+    end
+
+    test "given :gregorian, when some gregorian timestamp, returns result with converted value" do
+      result =
+        Result.new()
+        |> Result.set_value(63_755_511_991)
+        |> DateTime.check(:allow_int, :gregorian, Context.new("."))
+
+      assert result.value == ~U[2020-05-01 00:26:31Z]
+    end
+
+    test "given `false`, when some value, returns result with original value" do
+      result =
+        Result.new()
+        |> Result.set_value(1_464_096_368)
+        |> DateTime.check(:allow_int, false, Context.new("."))
+
+      assert result.value == 1_464_096_368
+    end
+  end
+
+  describe "Bliss.DateTime.check(_, :type, _, _)/4" do
+    test "given empty options, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> DateTime.check(:type, [], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given empty options, when DateTime value, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2020-05-01 00:26:31Z])
+        |> DateTime.check(:type, [], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given empty options, when non-DateTime value, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(123)
+        |> DateTime.check(:type, [], Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_type(),
+               message: "input is not a DateTime",
+               path: ["."]
+             })
+    end
+
+    test "given empty options, when NaiveDateTime value, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(~N[2000-01-01 23:00:07])
+        |> DateTime.check(:type, [], Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_type(),
+               message: "input is not a DateTime",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.DateTime.check(_, :shift, _, _)/4" do
+    test "given Etc/UTC, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> DateTime.check(:shift, "Etc/UTC", Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given Etc/UTC, when not a DateTime, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(123)
+        |> DateTime.check(:shift, "Etc/UTC", Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given Etc/UTC, when some DateTime with offset, returns result with shifted value" do
+      result =
+        Result.new()
+        |> Result.set_value(%Elixir.DateTime{
+          year: 2000,
+          month: 2,
+          day: 29,
+          zone_abbr: "CET",
+          hour: 23,
+          minute: 0,
+          second: 7,
+          microsecond: {0, 0},
+          utc_offset: 3600,
+          std_offset: 0,
+          time_zone: "Europe/Warsaw"
+        })
+        |> DateTime.check(:shift, "Etc/UTC", Context.new("."))
+
+      assert result.value == ~U[2000-02-29 22:00:07Z]
+    end
+
+    test "given invalid timezone, when some DateTime, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2020-05-01 00:26:31Z])
+        |> DateTime.check(:shift, "Europe/Warsaw", Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_arguments(),
+               message: "unable to shift input to \"Europe/Warsaw\" timezone",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.DateTime.check(_, :trunc, _, _)/4" do
+    test "given `true`, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> DateTime.check(:trunc, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when not a DateTime, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(123)
+        |> DateTime.check(:trunc, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some DateTime, returns result with value truncated to seconds" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2020-05-01 00:26:31.123456Z])
+        |> DateTime.check(:trunc, true, Context.new("."))
+
+      assert result.value == ~U[2020-05-01 00:26:31Z]
+    end
+
+    test "given :second, when some DateTime, returns result with value truncated to seconds" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2020-05-01 00:26:31.123456Z])
+        |> DateTime.check(:trunc, :second, Context.new("."))
+
+      assert result.value == ~U[2020-05-01 00:26:31Z]
+    end
+
+    test "given :millisecond, when some DateTime, returns result with value truncated to milliseconds" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2020-05-01 00:26:31.123456Z])
+        |> DateTime.check(:trunc, :millisecond, Context.new("."))
+
+      assert result.value == ~U[2020-05-01 00:26:31.123Z]
+    end
+
+    test "given :microsecond, when some DateTime, returns result with value truncated to microseconds" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2020-05-01 00:26:31.123456789Z])
+        |> DateTime.check(:trunc, :microsecond, Context.new("."))
+
+      assert result.value == ~U[2020-05-01 00:26:31.123456Z]
+    end
+  end
+
+  describe "Bliss.DateTime.check(_, :min, _, _)/4" do
+    test "given min DateTime, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> DateTime.check(:min, ~U[2000-01-01 00:00:00Z], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given min DateTime, when not a DateTime, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(123)
+        |> DateTime.check(:min, ~U[2000-01-01 00:00:00Z], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given min DateTime, when DateTime late enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2000-01-01 00:00:00Z])
+        |> DateTime.check(:min, ~U[2000-01-01 00:00:00Z], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given min DateTime, when DateTime too early, returns invalid result with errors" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[1999-12-31 23:59:59Z])
+        |> DateTime.check(:min, ~U[2000-01-01 00:00:00Z], Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.too_small(),
+               message: "input is too early",
+               path: ["."]
+             })
+    end
+
+    test "given invalid min DateTime, when some DateTime, returns invalid result with errors" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2000-01-01 00:00:00Z])
+        |> DateTime.check(:min, "2000-01-01 00:00:00Z", Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_arguments(),
+               message: "min value must be a DateTime",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.DateTime.check(_, :max, _, _)/4" do
+    test "given min DateTime, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> DateTime.check(:max, ~U[2000-01-01 00:00:00Z], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given min DateTime, when not a DateTime, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(123)
+        |> DateTime.check(:max, ~U[2000-01-01 00:00:00Z], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given min DateTime, when DateTime early enough, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2000-01-01 00:00:00Z])
+        |> DateTime.check(:max, ~U[2000-01-01 00:00:00Z], Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given min DateTime, when DateTime too late, returns invalid result with errors" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2000-01-01 00:00:01Z])
+        |> DateTime.check(:max, ~U[2000-01-01 00:00:00Z], Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.too_big(),
+               message: "input is too late",
+               path: ["."]
+             })
+    end
+
+    test "given invalid min DateTime, when some DateTime, returns invalid result with errors" do
+      result =
+        Result.new()
+        |> Result.set_value(~U[2000-01-01 00:00:00Z])
+        |> DateTime.check(:max, "2000-01-01T00:00:00Z", Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_arguments(),
+               message: "max value must be a DateTime",
+               path: ["."]
+             })
+    end
+  end
+
+  describe "Bliss.DateTime.validate/3" do
+    test "given some valid string, when :parse and other rules, set parsed value" do
+      {:ok, ~U[2000-01-01 00:00:00Z]} = DateTime.validate("2000-01-01T00:00:00Z", [:parse])
+
+      {:ok, ~U[2000-01-01 00:00:00Z]} =
+        DateTime.validate("2000-01-01T00:00:00Z", [:required, :parse])
+
+      {:ok, ~U[2000-01-01 00:00:00Z]} =
+        DateTime.validate("2000-01-01T00:00:00Z", [
+          :required,
+          :parse,
+          min: ~U[2000-01-01 00:00:00Z],
+          max: ~U[2000-01-01 00:00:00Z]
+        ])
+
+      {:ok, ~U[2000-01-01 00:00:00Z]} =
+        DateTime.validate("2000-01-01T00:00:00.123456Z", [
+          :required,
+          :parse,
+          :trunc
+        ])
+    end
+
+    test "given some valid int, when :allow_int, set converted value" do
+      {:ok, ~U[2000-01-01 00:00:00Z]} = DateTime.validate(946_684_800, [:allow_int])
+      {:ok, ~U[2000-01-01 00:00:00Z]} = DateTime.validate(63_113_904_000, allow_int: :gregorian)
+    end
+
+    test "given some valid DateTime, when :shift, set shifted value" do
+      {:ok, ~U[2000-01-01 00:00:00Z]} =
+        DateTime.validate(
+          %Elixir.DateTime{
+            year: 2000,
+            month: 1,
+            day: 1,
+            zone_abbr: "CET",
+            hour: 1,
+            minute: 0,
+            second: 0,
+            microsecond: {0, 0},
+            utc_offset: 3600,
+            std_offset: 0,
+            time_zone: "Europe/Warsaw"
+          },
+          shift: "Etc/UTC"
+        )
+    end
+  end
+end
