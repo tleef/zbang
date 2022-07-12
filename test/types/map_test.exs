2d8b669d5cb38339cbb9b8ad7635ed48d6a10d45
Thomas Lee <tom@tleef.com>
2022-07-12 00:39:15 -0400

    Add :atomize_keys tests

--- test/types/map_test.exs
+++ test/types/map_test.exs
@@ -64,4 +64,66 @@ defmodule Bliss.Map.Test do
       assert result.status == :valid
     end
   end
+
+  describe "Bliss.Map.check(_, :atomize_keys, _, _)/4" do
+    test "given `true`, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> Map.check(:atomize_keys, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when some non-map, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(123)
+        |> Map.check(:atomize_keys, true, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given `true`, when map with string key and atom doesn't exist, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(%{"oops" => "doesn't exist"})
+        |> Map.check(:atomize_keys, true, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_string(),
+               message: "unable to atomize key",
+               path: [".", "oops"]
+             })
+    end
+
+    test "given `true`, when map with string keys and atoms exist, returns valid result with atomized map" do
+      _ = :foo
+      _ = :bar
+
+      result =
+        Result.new()
+        |> Result.set_value(%{"foo" => "hello", "bar" => "world"})
+        |> Map.check(:atomize_keys, true, Context.new("."))
+
+      assert result.status == :valid
+
+      assert result.value == %{foo: "hello", bar: "world"}
+    end
+
+    test "given `true`, when map with mixed keys, returns valid result with atomized map" do
+      _ = :foo
+
+      result =
+        Result.new()
+        |> Result.set_value(%{"foo" => "foo", 2 => "two", bar: "bar"})
+        |> Map.check(:atomize_keys, true, Context.new("."))
+
+      assert result.status == :valid
+
+      assert result.value == %{2 => "two", foo: "foo", bar: "bar"}
+    end
+  end
 end
