fe4952ca239babcc11f7a20b4727c8e5ce1a52cc
Thomas Lee <tom@tleef.com>
2022-07-12 00:54:55 -0400

    Add :size tests

--- test/types/map_test.exs
+++ test/types/map_test.exs
@@ -126,4 +126,78 @@ defmodule Bliss.Map.Test do
       assert result.value == %{2 => "two", foo: "foo", bar: "bar"}
     end
   end
+
+  describe "Bliss.Map.check(_, :size, _, _)/4" do
+    test "given length, when nil, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(nil)
+        |> Map.check(:size, 6, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given length, when not a map, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value("Im a map")
+        |> Map.check(:size, 6, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given size, when too small, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(%{foo: "one", bar: "two", baz: "three"})
+        |> Map.check(:size, 4, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.too_small(),
+               message: "input does not have correct size",
+               path: ["."]
+             })
+    end
+
+    test "given size, when too big, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(%{foo: "one", bar: "two", baz: "three"})
+        |> Map.check(:size, 2, Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.too_big(),
+               message: "input does not have correct size",
+               path: ["."]
+             })
+    end
+
+    test "given size, when correct size, returns valid result" do
+      result =
+        Result.new()
+        |> Result.set_value(%{foo: "one", bar: "two", baz: "three"})
+        |> Map.check(:size, 3, Context.new("."))
+
+      assert result.status == :valid
+    end
+
+    test "given invalid size, when some value, returns invalid result with error" do
+      result =
+        Result.new()
+        |> Result.set_value(%{foo: "one", bar: "two", baz: "three"})
+        |> Map.check(:size, "3", Context.new("."))
+
+      assert result.status == :invalid
+
+      assert Enum.member?(result.errors, %Error{
+               code: Error.Codes.invalid_arguments(),
+               message: "unable to check size with size: \"3\", size must be an integer",
+               path: ["."]
+             })
+    end
+  end
 end
